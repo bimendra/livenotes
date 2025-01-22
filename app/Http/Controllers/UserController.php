4a7b0795672d4ce200f8fc0ea05e74524c4e8683
= <=>
2025-01-23 07:23:41 +0800

    User endpoints

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,47 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Repositories\UserRepository;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        return UserResource::collection(User::paginate($request->page_size ?? 20));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request, UserRepository $repository)
     {
-        //
+        return new UserResource($repository->create($request->only(['name', 'email', 'password'])));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        return new UserResource($user);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user, UserRepository $repository)
     {
-        //
+        return new UserResource($repository->update($user, $request->only(['name', 'email', 'password'])));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user, UserRepository $repository)
     {
-        //
+        $repository->forceDelete($user);
+        return new \Illuminate\Http\JsonResponse([
+            'data' => 'success',
+        ]);
     }
 }
